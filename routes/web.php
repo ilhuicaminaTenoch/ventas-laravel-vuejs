bd1fb7b92b60a0fc3ece1756f01d6eed641062b4
manuel moreno <manuel.moreno@televisatim.com>
2018-06-11 18:44:04 -0500

    Se actualiza archvios para mostrar los datos de crud

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,9 @@ Route::get('/', function () {
     return view('contenido/contenido');
 });
 
+
+Route::get('/categoria','CategoriaController@index');
+Route::post('/categoria/registrar','CategoriaController@store');
+Route::put('/categoria/registrar','CategoriaController@update');
+Route::put('/categoria/activar','CategoriaController@activar');
+Route::put('/categoria/desactivar','CategoriaController@desactivar');
